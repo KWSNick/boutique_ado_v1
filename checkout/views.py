42cbd37fe85df4fdfc2c19e2172f0fc36ecbf26d
Nicholas Bowley <nicholasbowley@hotmail.com>
2021-03-07 10:15:10 +0000

    Added stripe elements.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ISIjdJKzBnf14eftOwBmvbAcDGXaTHCQwZL5XYPCTRJQMvzNoNgNanwrTqO7Iqo3D86nPJ8sCFPPVfBj47ORQcN00Eg9cwcZ7',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
